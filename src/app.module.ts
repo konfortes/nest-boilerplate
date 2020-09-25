b692a753bd73d6c658739bec5ce53f9292ed6280
konfortes <ronen.konfortes@riskified.com>
2020-09-26 00:54:13 +0300

    logging module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,10 @@
 import { OrderModule } from './order/order.module'
 import { Module } from '@nestjs/common'
 
-import {
-  WinstonModule,
-  WinstonModuleOptions,
-  utilities as nestWinstonModuleUtilities,
-} from 'nest-winston'
 import { HealthModule } from './health/health.module'
-import * as winston from 'winston'
+import { LoggingModule } from './logging/logging.module'
 
 @Module({
-  imports: [WinstonModule.forRoot(loggerConfig()), HealthModule, OrderModule],
+  imports: [HealthModule, LoggingModule, OrderModule],
 })
 export class AppModule {}
-
-function loggerConfig(): WinstonModuleOptions {
-  const env: string = process.env.NODE_ENV || 'development'
-  return {
-    level: process.env.LOG_LEVEL || 'info',
-    transports: transportConfig(env),
-  }
-}
-
-function transportConfig(env: string) {
-  if (env == 'development') {
-    return new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        nestWinstonModuleUtilities.format.nestLike(),
-      ),
-    })
-  } else {
-    return new winston.transports.Console({
-      format: winston.format.json(),
-    })
-  }
-}
---
+++ src/logging/logging.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common'
+import {
+  WinstonModule,
+  WinstonModuleOptions,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston'
+import * as winston from 'winston'
+
+@Module({
+  imports: [WinstonModule.forRoot(loggerConfig())],
+})
+export class LoggingModule {}
+
+function loggerConfig(): WinstonModuleOptions {
+  const env: string = process.env.NODE_ENV || 'development'
+  return {
+    level: process.env.LOG_LEVEL || 'info',
+    transports: transportConfig(env),
+  }
+}
+
+function transportConfig(env: string) {
+  if (env === 'development') {
+    return devTransport()
+  } else {
+    return prodTransport()
+  }
+}
+
+function devTransport() {
+  return new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      nestWinstonModuleUtilities.format.nestLike(),
+    ),
+  })
+}
+
+function prodTransport() {
+  return new winston.transports.Console({
+    format: winston.format.json(),
+  })
+}
